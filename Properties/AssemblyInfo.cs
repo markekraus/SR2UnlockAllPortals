f00f9e0b1d1ef8033009290ceabe5808017f9465
Mark Kraus <github@markekraus.com>
2023-11-19 13:55:34 -0800

    Initial release

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using UnlockAllPortals;
+using MelonLoader;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: MelonAuthorColor(255, 255, 0, 188)]
+[assembly: MelonColor(255, 187, 255, 255)]
+[assembly: MelonInfo(typeof(Entry), "Unlock All Portals", "1.0.0", "MarkEKraus", null)]
+[assembly: MelonGame("MonomiPark", "SlimeRancher2")]
+[assembly: AssemblyTitle("UnlockAllPortals")]
+[assembly: AssemblyDescription("Slime Rancher 2 mod that unlocks all portals.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mark E. Kraus")]
+[assembly: AssemblyProduct("UnlockAllPortals")]
+[assembly: AssemblyCopyright("Copyright © Mark E. Kraus 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("91517a8d-849b-4d5e-8cde-2598a1684183")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
---
+++ UnlockAllPortals.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Il2Cpp;
+using Il2CppMonomiPark.SlimeRancher.DataModel;
+using Il2CppMonomiPark.World;
+using MelonLoader;
+using UnityEngine;
+
+namespace UnlockAllPortals
+{
+    internal class Entry : MelonMod
+    {
+        public override void OnSceneWasUnloaded(int buildIndex, string sceneName)
+        {
+            if (sceneName != "LoadScene")
+                return;
+
+            foreach (var item in Resources.FindObjectsOfTypeAll<WorldStatePrimarySwitch>())
+            {
+                if(item._model.state == SwitchHandler.State.DOWN)
+                    return;
+                
+                item._model.state = SwitchHandler.State.DOWN;
+                item.SetStateForAll(SwitchHandler.State.DOWN, true);
+            }
+        }
+    }
+}
